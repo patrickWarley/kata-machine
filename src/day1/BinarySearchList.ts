56c38a373659b52d8cbb0ce26c942bf77feebc6c
Patrick Warley <patrickwarley@gmail.com>
2023-07-12 22:44:49 -0300

    Binary search implemented

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+	let low = 0;
+	let high = haystack.length;
 
+	do {
+		const middle = Math.floor(low + (high - low) / 2);
+		const n = haystack[middle];
+
+		if (n === needle) return true;
+
+		if (n < needle) {
+			low = middle + 1;
+		} else {
+			high = middle;
+		}
+	} while (low < high)
+
+
+	return false;
 }
