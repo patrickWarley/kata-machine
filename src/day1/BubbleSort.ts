f43e31537ee90b5871009255ca098198c30d920d
Patrick <patrickwarley@gmail.com>
2023-08-23 21:59:22 -0300

    Bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,27 @@
 export default function bubble_sort(arr: number[]): void {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				const tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+export function bubble_sortMyFirstImplementation(arr: number[]): void {
+
+	//it works but it's not perfect
+	//the biggest difference is that 
+	for (let arrlen = arr.length; arrlen > 1; arrlen--) {
+		for (let j = 0; j < arrlen; j++) {
+			if (arr[j] > arr[j + 1]) {
+				let aux = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = aux;
+			}
+		}
+	}
 
 }
