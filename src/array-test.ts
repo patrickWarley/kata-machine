7d89bc0d4fe2704dfb59fb72a19ac5d0baac1274
Patrick Warley <patrickwarley@gmail.com>
2023-09-10 16:45:47 -0300

    MazeSOlver

--- src/array-test.ts
+++ src/array-test.ts
@@ -1,102 +1,123 @@
 
-const a: number[] = [];
+const a: number[] = []; // Is this an array?
+
+/**
+ * No it's not an array. How do we know that?
+ * - We know that because an array is a contiguous memory space
+ * that's allocated at the moment of definition it has to have a fixed size.
+ * And we know that is not the case in javascript.
+ * So javascript is using another data structure under the hood. Which one?
+ * 
+ * The whole code below is to try and define this data structure.
+ */
 
 function time(fn: () => void): number {
-    const now = Date.now();
-    fn();
-    return Date.now() - now;
+	const now = Date.now();
+	fn();
+	return Date.now() - now;
 }
 
 function unshift(number: number) {
-    for (let i = 0; i < number; ++i) {
-        a.unshift(Math.random());
-    }
+	for (let i = 0; i < number; ++i) {
+		a.unshift(Math.random());
+	}
 }
 
 function shift(number: number) {
-    for (let i = 0; i < number; ++i) {
-        a.shift();
-    }
+	for (let i = 0; i < number; ++i) {
+		a.shift();
+	}
 }
 
 function push(number: number) {
-    for (let i = 0; i < number; ++i) {
-        a.push(Math.random());
-    }
+	for (let i = 0; i < number; ++i) {
+		a.push(Math.random());
+	}
 }
 
 function pop(number: number) {
-    for (let i = 0; i < number; ++i) {
-        a.pop();
-    }
+	for (let i = 0; i < number; ++i) {
+		a.pop();
+	}
 }
 
 function get(idx: number) {
-    return function() {
-        return a[idx];
-    };
+	return function () {
+		return a[idx];
+	};
 }
 
 function push_arr(count: number) {
-    return function() {
-        push(count);
-    };
+	return function () {
+		push(count);
+	};
 }
 
 function pop_arr(count: number) {
-    return function() {
-        pop(count);
-    };
+	return function () {
+		pop(count);
+	};
 }
 
 function unshift_arr(count: number) {
-    return function() {
-        unshift(count);
-    };
+	return function () {
+		unshift(count);
+	};
 }
 
 function shift_arr(count: number) {
-    return function() {
-        shift(count);
-    };
+	return function () {
+		shift(count);
+	};
 }
 
 const tests = [10, 100, 1000, 10000, 100000, 1_000_000, 10_000_000];
 console.log("Testing get");
 tests.forEach(t => {
-    a.length = 0;
-    push(t);
-    console.log(t, time(get(t - 1)));
+	a.length = 0;
+	push(t);
+	console.log(t, time(get(t - 1)));
 });
 
 console.log("push");
 tests.forEach(t => {
-    a.length = 0;
-    push(t);
+	a.length = 0;
+	push(t);
 
-    console.log(t, time(push_arr(1000)));
+	console.log(t, time(push_arr(1000)));
 });
 
 console.log("pop");
 tests.forEach(t => {
-    a.length = 0;
-    push(t);
+	a.length = 0;
+	push(t);
 
-    console.log(t, time(pop_arr(1000)));
+	console.log(t, time(pop_arr(1000)));
 });
 
 console.log("unshift");
 tests.forEach(t => {
-    a.length = 0;
-    push(t);
+	a.length = 0;
+	push(t);
 
-    console.log(t, time(unshift_arr(1000)));
+	console.log(t, time(unshift_arr(1000)));
 });
 
 console.log("shift");
 tests.forEach(t => {
-    a.length = 0;
-    push(t);
+	a.length = 0;
+	push(t);
 
-    console.log(t, time(shift_arr(1000)));
+	console.log(t, time(shift_arr(1000)));
 });
+
+
+/**
+ * And the answer is: ArrayList
+ * Push/Pop/get(idx) = O(1)
+ * shift/unshift = O(n)
+ * 
+ * 
+ * shift = remove from the start
+ * unshift = add to the start
+ */
--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,102 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+function comparePoints(a: Point, b: Point) {
+	return (a.x === b.x && b.y === a.y);
+}
 
+function checkOutOfBounds(maze: String[], point: Point) {
+	const y = maze.length - 1;
+	const x = maze[0].length - 1;
+
+	return (point.x > x || point.y > y) || (point.x < 0 || point.y < 0);
+}
+
+//The problem with my implementations was the fact that I wasn't checking if 
+// I was passing on the same spot, this was causing my code to get in an infinite loop.
+
+//When working with recursion remember : Base cases => pre, recursion, pos
+//think about creating another function to do the recursion
+export function solve_My_Implementation(maze: string[], wall: string, end: Point, start: Point): Point[] {
+	//Think about the base case
+	//When do you stop?
+	// 	- Going where you Already have gone before
+
+	//finding the end
+	if (comparePoints(start, end)) return [start];
+
+
+	//going out of bounds
+	else if (checkOutOfBounds(maze, start)) return [];
+
+	//hitting a wall
+	else if (maze[start.y][start.x] === wall) return [];
+
+	let point = solve_My_Implementation(maze, wall, { x: start.x + 1, y: start.y }, end);
+	if (point.length === 0) {
+		point = solve_My_Implementation(maze, wall, { x: start.x - 1, y: start.y }, end);
+		if (point.length === 0) {
+			point = solve_My_Implementation(maze, wall, { x: start.x, y: start.y + 1 }, end);
+			if (point.length === 0) {
+				point = solve_My_Implementation(maze, wall, { x: start.x, y: start.y - 1 }, end)
+			}
+		}
+	}
+
+	point.push(start);
+
+	return point;
+}
+
+const dir = [
+	[0, -1], [0, 1], [-1, 0], [1, 0]
+];
+
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]): boolean {
+
+	//Base cases
+	//out of bounds
+	if (checkOutOfBounds(maze, curr)) return false;
+
+	//broken nose
+	if (maze[curr.y][curr.x] === wall) return false;
+
+	//going over the same spot
+	if (seen[curr.y][curr.x]) return false;
+
+	//Got to the end
+	if (comparePoints(curr, end)) {
+		path.push(end);
+		return true;
+	}
+
+	//pre conditions
+	// I have seen this point and for now he is part of the solution
+	seen[curr.y][curr.x] = true;
+	path.push(curr);
+
+	//recurssion
+	for (let i = 0; i < dir.length; i++) {
+		let [x, y] = dir[i];
+		if (walk(maze, wall, { x: curr.x + x, y: curr.y + y }, end, seen, path)) {
+			return true;
+		}
+	}
+
+	//pos conditions
+	//the latest point is not part of the solution
+	path.pop();
+
+	//there's no solution
+	return false;
+};
+
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+	let path: Point[] = [];
+	let seen: boolean[][] = [];
+
+	for (let i = 0; i < maze.length; i++) {
+		seen.push(new Array(maze[0].length).fill(false));
+	}
+
+	walk(maze, wall, start, end, seen, path);
+
+	return path;
 }
