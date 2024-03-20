5d1c34ddd251731636344f0223c2a15315dc18a7
patrick <patrickwarley@gmail.com>
2024-03-20 16:10:14 -0300

    DFSGraphList implemented

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,47 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    let path = new Array(graph.length).fill(-1);
+    let seen = new Array(graph.length).fill(false);
 
+    let queue = [];
+
+    queue.push(source);
+
+    let curr;
+
+    do {
+        curr = queue.shift() as number;
+
+        seen[curr] = true;
+
+        if (curr === needle)
+            break;
+
+        let adjs = graph[curr];
+        
+        for (let i = 0; i < adjs.length; i++) {
+            
+            let adj: number = adjs[i];
+            if(adj === 0) continue;
+
+            if (seen[i]) continue;
+
+            queue.push(i);
+            path[i] = curr;
+        }
+    } while (queue.length > 0)
+    
+        if (curr === needle) {
+        let result = [curr];
+        
+        while (curr !== -1) {
+            let node: number = path[curr];
+
+            if (node !== -1) result.push(node);
+            curr = node;
+        }
+
+        return result.reverse();
+    }
+
+    return null;
 }
--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,52 +1,89 @@
+function walk(
+    graph: WeightedAdjacencyList,
+    curr: number,
+    needle: number,
+    seen: boolean[],
+    path: number[]
+): boolean {
+    if (seen[curr]) return false;
+
+    path.push(curr);
+    if (curr === needle) return true
+
+    seen[curr] = true;
+    let adjs = graph[curr];
+    let result;
+    for (let i = 0; i < adjs.length; i++) {
+        result = walk(graph, adjs[i].to, needle, seen, path);
+        if (result) break;
+    }
+    
+    if(!result){
+        path.pop()
+        return false;
+    }
+
+    return true;
+}
+
 export default function dfs(
     graph: WeightedAdjacencyList,
     source: number,
     needle: number
 ): number[] | null {
-    let path = new Array(graph.length);
-    let seen = new Array(graph.length);
-    let queue = [];
+    
+    let seen:boolean[] = new Array(graph.length).fill(false);
+    let path:number[] = [];
 
-    path.fill(-1);
-    seen.fill(false);
+    let result = walk(graph, source, needle, seen, path);
+    return result?path:null;
+}
+
+
+//I did it using interation, but the master did using recurssion
+//:D wtf!
+function dfs_my_implementation(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number
+): number[] | null {
+
+    let seen = new Array(graph.length).fill(false);
+    let path = new Array(graph.length).fill(-1);
+    let queue = [];
 
     queue.push(source);
     let curr;
 
     do {
-        curr = queue.shift();
+        curr = queue.shift() as number;
+        seen[curr] = true;
+        if (curr === needle) break;
 
-        if (curr !== undefined) {
-            seen[curr] = true;
+        let adjs = graph[curr];
 
-            if (curr === needle)
-                break;
+        for (let i = 0; i < adjs.length; i++) {
+            let adj: number = adjs[i].to;
 
-            let children = graph[curr];
-            
-            if(children === null) return null;
+            if (seen[adj]) continue;
 
-            for (let i = 0; i < children.length; i++) {
-                    let child:number = children[i].to;
-                if (seen[child]) continue;
+            path[adj] = curr;
 
-                queue.push(child);
-                path[child] = curr;
-            }
+            queue.push(adj);
         }
-    } while (queue.length > 0)
+    } while (queue.length)
+
 
     if (curr === needle) {
-        let result = [curr];
+        let result = [];
         while (curr !== -1) {
-            let node:number = path[curr];
-
-            if(node!==-1)result.push(node);
-            curr = node;
+            result.push(curr);
+            curr = path[curr];
         }
 
         return result.reverse();
     }
+
 
     return null;
 }
